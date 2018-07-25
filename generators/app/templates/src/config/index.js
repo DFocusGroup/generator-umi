576b559199073da011dbd918e38ff7bd6fdc0fad
Howard.Zuo <leftstick@qq.com>
2018-07-25 11:20:52 +0800

    refactor: ajust config position

--- generators/app/templates/src/config/index.js
+++ generators/app/templates/src/config/index.js
@@ -1,7 +1,7 @@
 import { isProduction } from '../helpers/env'
 
 /**
- * 所有的 *_PLACEHOLDER，在npm run serve启动时，都需要指定运行时需要的值
+ * 所有的 *_PLACEHOLDER__，在npm run serve启动时，都需要指定运行时需要的值
  * 这个在“Use docker for release”时非常好用，可以针对不同的运行环境使用同一个镜像，指定不同的环境变量
  */
 
@@ -9,8 +9,8 @@ export const OPEN_PAGES = ['/login', '/', '/404']
 
 export const STORAGE_TOKEN_KEY = 'platform-token'
 
-export const STORAGE_DOMAIN = !isProduction ? window.location.hostname : 'STORAGE_DOMAIN_PLACEHOLDER__'
-
 export const STORAGE_EXPIRE_DAYS = 49
+
+export const STORAGE_DOMAIN = !isProduction ? window.location.hostname : 'STORAGE_DOMAIN_PLACEHOLDER__'
 
 export const API_HOST = !isProduction ? '/' : 'API_HOST_PLACEHOLDER__'
