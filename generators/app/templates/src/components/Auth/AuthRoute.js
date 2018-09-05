e24960efd84cd58e304b603ddfeffe329fb6e5b2
Howard.Zuo <leftstick@qq.com>
2018-09-05 16:47:12 +0800

    fix: adjust authroute usage for new umi

--- generators/app/templates/src/components/Auth/AuthRoute.js
+++ generators/app/templates/src/components/Auth/AuthRoute.js
@@ -1,38 +1,26 @@
-import { connect } from 'dva'
-import { Route, Redirect } from 'dva/router'
+import { Redirect } from 'dva/router'
 import PropTypes from 'prop-types'
 
 import { getToken } from '../../helpers/storage'
 
 function AuthRoute(props) {
-  const { render, location, ...rest } = props
-  return (
-    <Route
-      {...rest}
-      render={props => {
-        if (!getToken()) {
-          return (
-            <Redirect
-              to={{
-                pathname: '/login',
-                search: `?from=${encodeURIComponent(location.pathname)}`
-              }}
-            />
-          )
-        }
-        return render(props)
-      }}
-    />
-  )
+  const { children, location } = props
+  if (!getToken()) {
+    return (
+      <Redirect
+        to={{
+          pathname: '/login',
+          search: `?from=${encodeURIComponent(location.pathname)}`
+        }}
+      />
+    )
+  }
+  return children
 }
 
 AuthRoute.propTypes = {
-  render: PropTypes.func,
+  children: PropTypes.any,
   location: PropTypes.object
 }
 
-export default connect(({ users }) => {
-  return {
-    users
-  }
-})(AuthRoute)
+export default AuthRoute
