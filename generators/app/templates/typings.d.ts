dd0a4ab4d7e1abd2cb0dd3d272122e9908529fdf
Howard.Zuo <leftstick@qq.com>
2020-04-30 11:20:35 +0800

    feat: improve .svg usage

--- generators/app/templates/typings.d.ts
+++ generators/app/templates/typings.d.ts
@@ -2,4 +2,6 @@ declare module '*.css'
 declare module '*.less'
 declare module '*.png'
 declare module '*.jpg'
-declare module '*.svg'
+declare module '*.svg' {
+  export function ReactComponent(props: React.SVGProps<SVGSVGElement>): React.ReactElement
+}
