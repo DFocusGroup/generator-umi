ab2c01372bc117c8ed2dbb43fbb8d7b2c8c32291
Howard.Zuo <leftstick@qq.com>
2020-05-06 15:39:14 +0800

    feat: enhance way of svg usage

--- generators/app/templates/typings.d.ts
+++ generators/app/templates/typings.d.ts
@@ -4,4 +4,6 @@ declare module '*.png'
 declare module '*.jpg'
 declare module '*.svg' {
   export function ReactComponent(props: React.SVGProps<SVGSVGElement>): React.ReactElement
+  const url: string
+  export default url
 }
