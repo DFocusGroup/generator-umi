1d8c3bfa20e1d842c242f35426960765f2b3bc8b
Howard.Zuo <leftstick@qq.com>
2020-07-21 16:48:51 +0800

    fix: remove unused theme config

--- generators/app/templates/config/config.ts
+++ generators/app/templates/config/config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'umi'
 
-import theme from './theme'
-
 export default defineConfig({
   hash: true,
   mock: {
     exclude: ['mock/**/_*.[jt]s']
   },
   antd: {},
-  theme,
   locale: {
     antd: true,
     title: true
--- generators/app/templates/config/theme.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  '@primary-color': '#1991eb'
-}
