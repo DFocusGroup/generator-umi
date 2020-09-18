96f05db247e16b03072126b5d37b961ae99db373
Howard.Zuo <leftstick@qq.com>
2020-09-18 13:44:30 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "175234f5f1341550b6bcd41e62bc7cf6"
+    "revision": "dc21ef9576ec2a1093394230ee18f608"
   },
   {
     "url": "assets/css/0.styles.952b84a7.css",
@@ -38,16 +38,16 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.d586e96e.js",
-    "revision": "f933948853fe2ca2ae8aae97a09be676"
+    "url": "assets/js/10.666d0345.js",
+    "revision": "603b1aa8ce709ac5c7989c87805c4361"
   },
   {
-    "url": "assets/js/11.54c6fadb.js",
-    "revision": "7fc0b4a788f92e07a72542046976f690"
+    "url": "assets/js/11.4d0eddbe.js",
+    "revision": "b456913e5ab73b026b93a84da1583346"
   },
   {
-    "url": "assets/js/12.e8e560dc.js",
-    "revision": "39c03a81c36e1a07672868ffb5fa82f0"
+    "url": "assets/js/12.ad2d4b14.js",
+    "revision": "46c2c3bdb7da6d3623404398276d594d"
   },
   {
     "url": "assets/js/13.ad6599e0.js",
@@ -74,24 +74,24 @@ self.__precacheManifest = [
     "revision": "b77fdd367d9248cd260e6c7a6e4ab6b3"
   },
   {
-    "url": "assets/js/6.a58bb5ea.js",
-    "revision": "4dafea89cb708f7bf227580103c6a36f"
+    "url": "assets/js/6.31c47903.js",
+    "revision": "ee1d3e0adb5473811c2bea84463daf6c"
   },
   {
-    "url": "assets/js/7.3f66b0a7.js",
-    "revision": "652789718c686098123ac3d4d375c0b9"
+    "url": "assets/js/7.c5f6610e.js",
+    "revision": "afa8355aee34379547a4afd5a6372143"
   },
   {
     "url": "assets/js/8.b4b6386c.js",
     "revision": "e3868f3ab0cddffa715d9fffbb6fcce2"
   },
   {
-    "url": "assets/js/9.8aa4b6cc.js",
-    "revision": "c09d5cb545ac2b1b77a8c59b2049b88f"
+    "url": "assets/js/9.4153c022.js",
+    "revision": "a55f27652eff60c709dfc97c59a01baf"
   },
   {
-    "url": "assets/js/app.0d46703b.js",
-    "revision": "e058c960662d7004908f48cfd9548011"
+    "url": "assets/js/app.4fcc90df.js",
+    "revision": "68a0529fe1c25cc4c330cb229a2c3cde"
   },
   {
     "url": "blank_layout.png",
@@ -107,35 +107,35 @@ self.__precacheManifest = [
   },
   {
     "url": "guide/data.html",
-    "revision": "360c0cff5abee7039bea5b6d1cf04931"
+    "revision": "ea3e4c6730aee2b49e9ecd9f43517420"
   },
   {
     "url": "guide/index.html",
-    "revision": "072d98981e5ce5b2e28db807afcafbde"
+    "revision": "a44e3db26c1181686d5395be563211b5"
   },
   {
     "url": "guide/layout.html",
-    "revision": "eb29db778f84e54357c4b86376b7052e"
+    "revision": "5808397003c1844aed3bd1d531e8bba6"
   },
   {
     "url": "guide/locale.html",
-    "revision": "6dd4c533efb457342ff0385ebb1e169e"
+    "revision": "90c48a81b6e6ff858c6a04bf1a2fdadd"
   },
   {
     "url": "guide/packaging.html",
-    "revision": "06e056fcf4d2f93745ac87f59aa44a3f"
+    "revision": "d22a3ce7b3c9ea7b3efae74e7c27efc0"
   },
   {
     "url": "guide/route.html",
-    "revision": "dc6751c3969ac9939576a6e052a27ef2"
+    "revision": "0f99a6eed839a36a81ffb522dcde59fe"
   },
   {
     "url": "guide/theme.html",
-    "revision": "81db33c5ed573ef6371dd20d16506b6a"
+    "revision": "8c8d5b1b110325cd078bf5e532eced35"
   },
   {
     "url": "index.html",
-    "revision": "dd5d473e72f99cfa9203989396b3564b"
+    "revision": "f09e39eed6cf478fbe91cc27807b0b60"
   },
   {
     "url": "locale.gif",
